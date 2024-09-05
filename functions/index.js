dc6e3db0e3ab9d7d96001954158bd3ea769d595a
Lucas Guellis de Souza <lucasguellis.souza@gmail.com>
2024-09-05 11:40:01 -0300

    feat: defining routes for users

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,50 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+const express = require("express");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const app = express();
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Fala Xunico!");
+app.get("/users", async (req, res) => {
+  try {
+    const usersSnapshot = await admin.firestore().collection("users").get();
+    if (usersSnapshot.empty) {
+      logger.info("No matching documents.");
+      res.status(404).json({message: "No users found."});
+      return;
+    }
+
+    const users = usersSnapshot.docs.map((doc) => doc.data());
+    res.status(200).json(users);
+  } catch (error) {
+    logger.error("Error retrieving users:", error);
+    res.status(500).json({error: "Failed to retrieve users"});
+  }
 });
+
+app.get("/users/:name", async (req, res) => {
+  try {
+    const {name} = req.params;
+    const usersSnapshot = await admin.firestore()
+        .collection("users")
+        .where("name", "==", name)
+        .get();
+
+    if (usersSnapshot.empty) {
+      logger.info(`No user found with the name: ${name}`);
+      res.status(404).json({message: `No user found with the name: ${name}`});
+      return;
+    }
+
+    const users = usersSnapshot.docs.map((doc) => doc.data());
+    res.status(200).json(users);
+  } catch (error) {
+    logger.error("Error retrieving user:", error);
+    res.status(500).json({error: "Failed to retrieve user"});
+  }
+});
+
+
+exports.app = onRequest(app);
