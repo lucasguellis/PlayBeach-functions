880cae204281e1fa0f58e1d6e4de4ae7b94e6a96
Lucas Guellis de Souza <lucasguellis.souza@gmail.com>
2024-09-23 07:32:11 -0300

    feat: adding subcollections to formatter

--- functions/utils/snapshotFormatter.js
+++ functions/utils/snapshotFormatter.js
@@ -1,11 +1,25 @@
-exports.formatObjectList = (snapshot) => {
+exports.formatObjectList = async (snapshot) => {
   if (snapshot.empty) return null;
-  return snapshot.docs.map((doc) => {
-    return {id: doc.id, ...doc.data()};
-  });
+  return Promise.all(snapshot.docs.map(async (doc) => {
+    const data = doc.data();
+    const subcollections = await getSubcollections(doc.ref);
+    return { id: doc.id, ...data, ...subcollections };
+  }));
 };
 
-exports.formatObject = (snapshot) => {
+exports.formatObject = async (snapshot) => {
   if (snapshot.empty) return null;
-  return {id: snapshot.id, ...snapshot.data()};
+  const data = snapshot.data();
+  const subcollections = await getSubcollections(snapshot.ref);
+  return { id: snapshot.id, ...data, ...subcollections };
 };
+
+async function getSubcollections(docRef) {
+  const collections = await docRef.listCollections();
+  const subcollections = {};
+  for (const collectionRef of collections) {
+    const snapshot = await collectionRef.get();
+    subcollections[collectionRef.id] = await exports.formatObjectList(snapshot);
+  }
+  return subcollections;
+}
